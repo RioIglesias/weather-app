245e96849293ff46a2e041663166a53d1da9d328
RioIglesias <rioiglesias03@gmail.com>
2023-10-09 14:56:44 +0700

    banner:false

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
